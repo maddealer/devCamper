6772c651abcf679f3a27eb6a59ac1beeec5880e7
maddealer <forento@gmail.com>
2020-11-09 18:00:07 +0200

    DB connected, model created CRUD bootcamps done

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-
-//Route files
-const bootcamps = require("./routes/bootcamps");
+const connectDB = require("./config/db");
 
 //load env vars
 dotenv.config({ path: "./config/config.env" });
 
+//Connect MongoDB
+connectDB();
+
+//Route files
+const bootcamps = require("./routes/bootcamps");
+
 const app = express();
+
+//Body Parser
+app.use(express.json());
 
 //Dev logging middleware MORGAN
 if (process.env.NODE_ENV === "development") {
@@ -20,7 +27,16 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+//Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //close server and exit
+  server.close(() => {
+    process.exit(1);
+  });
+});
